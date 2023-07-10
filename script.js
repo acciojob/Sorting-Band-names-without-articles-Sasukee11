de5913369f060667a03838f6a93aed65fb229a3f
Sasuke <118628015+Sasukee11@users.noreply.github.com>
2023-07-10 23:55:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 //your code here
+// suppose an array that contains a list of popular tourise spots
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
+touristSpots.sort();
